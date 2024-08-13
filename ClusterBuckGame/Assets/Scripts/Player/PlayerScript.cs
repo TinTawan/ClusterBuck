2c09806e33e5b440fcd43a32530ca7953dce625a
Justin <jw4566c@gre.ac.uk>
2024-08-13 21:56:16 +0100

    setting up follow cam
    cinemachine cam used to follow player and rotate around with mouse

--- ClusterBuckGame/Assets/Scripts/Player/PlayerScript.cs
+++ ClusterBuckGame/Assets/Scripts/Player/PlayerScript.cs
@@ -7,7 +7,7 @@ using UnityEngine.InputSystem;
 public class PlayerScript : NetworkBehaviour
 {
     private Rigidbody rb;
-
+    private Animator animator;
 
     [SerializeField] private float moveSpeed = 10f;
 
@@ -19,18 +19,42 @@ public class PlayerScript : NetworkBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+        animator = GetComponent<Animator>();
 
     }
 
     private void Update()
     {
-        moveDir = moveIAR.action.ReadValue<Vector2>();   
+        moveDir = moveIAR.action.ReadValue<Vector2>();
+
+        Animation();
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector3(moveDir.x * moveSpeed, 0, moveDir.y * moveSpeed);
 
+    }
+
+    void Animation()
+    {
+        /*if(rb.velocity.magnitude <= 0.2)
+        {
+            animator.SetBool("isWalking", false);
+        }
+        else
+        {
+            animator.SetBool("isWalking", true);
+        }*/
+
+
+        if(moveDir.magnitude >= 0.1)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
     }
 }
