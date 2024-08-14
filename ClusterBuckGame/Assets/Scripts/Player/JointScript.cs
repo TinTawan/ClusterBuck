140683840840847111f7c2ffca5e3d70e3663ddc
Justin <jw4566c@gre.ac.uk>
2024-08-14 16:58:37 +0100

    added walk and idle anims and linked to player joint rotation
    player limbs now rotate to copy the animated model's animations

---
+++ ClusterBuckGame/Assets/Scripts/Player/JointScript.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointScript : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private ConfigurableJoint joint;
+
+    [SerializeField] private Transform targetJoint;
+    [SerializeField] private Quaternion initRotation;
+
+    //[SerializeField] bool invertX, invertY, invertZ, swapXY, swapXZ, swapYZ;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        joint = GetComponent<ConfigurableJoint>();
+
+        initRotation = joint.transform.localRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        JointRotation();
+    }
+
+    void JointRotation()
+    {
+        /*Quaternion inRot = targetJoint.rotation;
+
+        if (invertX)
+        {
+            inRot *= Quaternion.Euler(180, 0, 0);
+        }
+        if (invertY)
+        {
+            inRot *= Quaternion.Euler(0, 180, 0);
+        }
+        if (invertZ)
+        {
+            inRot *= Quaternion.Euler(0, 0, 180);
+        }
+        if(swapXY)
+        {
+            inRot *= Quaternion.Euler(targetJoint.rotation.y, targetJoint.rotation.x, targetJoint.rotation.z);
+        }
+        if (swapXZ)
+        {
+            inRot *= Quaternion.Euler(targetJoint.rotation.z, targetJoint.rotation.y, targetJoint.rotation.x);
+        }
+        if (swapYZ)
+        {
+            inRot *= Quaternion.Euler(targetJoint.rotation.x, targetJoint.rotation.z, targetJoint.rotation.y);
+        }
+
+        
+
+        joint.targetRotation = inRot;*/
+
+
+        SetTargetRotation(joint, targetJoint.localRotation, initRotation, Space.Self);
+    }
+
+
+    void SetTargetRotation(ConfigurableJoint inJoint, Quaternion inTargetRot, Quaternion inInitRot, Space inSpace)
+    {
+        var right = inJoint.axis;
+        var forward = Vector3.Cross(inJoint.axis, inJoint.secondaryAxis).normalized;
+        var up = Vector3.Cross(forward, right).normalized;
+        Quaternion worldtoJointSpace = Quaternion.LookRotation(forward,up);
+
+        Quaternion resultRot = Quaternion.Inverse(worldtoJointSpace);
+
+        if(inSpace == Space.World)
+        {
+            resultRot *= inInitRot * Quaternion.Inverse(inTargetRot);
+            
+        }
+        else
+        {
+            resultRot *= Quaternion.Inverse(inTargetRot) * inInitRot;
+
+        }
+
+        resultRot *= worldtoJointSpace;
+
+        inJoint.targetRotation = resultRot;
+    }
+}
